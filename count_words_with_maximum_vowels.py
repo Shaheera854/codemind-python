0c250fc02c4bd368823f3be73e3b565068894f11
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-07-17 21:12:35 +0530

    def max_count(s):     s=s.split()     v=['a','e','i','o','u','A','E','I','O','U']     c=0     for i in range(len(s)):         m=c         c=0         for j in range(len(s[i])):             if s[i][j] in v:                 c+=1         if c<m:             c=m     return c s=input() m=max_count(s) s=s.split() co=0 v=['a','e','i','o','u','A','E','I','O','U'] for i in range(len(s)):     c=0     for j in range(len(s[i])):         if s[i][j] in v:             c+=1     if c==m:         co+=1 print(co)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,26 @@
+def max_count(s):
+    s=s.split()
+    v=['a','e','i','o','u','A','E','I','O','U']
+    c=0
+    for i in range(len(s)):
+        m=c
+        c=0
+        for j in range(len(s[i])):
+            if s[i][j] in v:
+                c+=1
+        if c<m:
+            c=m
+    return c
+s=input()
+m=max_count(s)
+s=s.split()
+co=0
+v=['a','e','i','o','u','A','E','I','O','U']
+for i in range(len(s)):
+    c=0
+    for j in range(len(s[i])):
+        if s[i][j] in v:
+            c+=1
+    if c==m:
+        co+=1
+print(co)
