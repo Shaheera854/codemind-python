97e4088b3e580834c76b6708fb5611385072487c
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-07-19 20:53:44 +0530

    s=input() s1=0 s=s.replace(' ','') for i in s:     c=0     for j in s:         if i==j or ord(i)+32==ord(j) or ord(i)-32==ord(j) and i!=' ' and j!=' ':             c+=1     if c==1:         s1+=1 print(s1)

---
+++ count_unique_characters.py
@@ -0,0 +1,11 @@
+s=input()
+s1=0
+s=s.replace(' ','')
+for i in s:
+    c=0
+    for j in s:
+        if i==j or ord(i)+32==ord(j) or ord(i)-32==ord(j) and i!=' ' and j!=' ':
+            c+=1
+    if c==1:
+        s1+=1
+print(s1)
