ab479e3d37fc9132dcc1770eb0df7cfb65cbd260
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-07-19 21:43:40 +0530

    s=input() s=str.lower(s) s=s.split() a=[] s1=s[0] for i in s1:     c=0     for j in s:         if i in j:             c+=1     if c==len(s):         a.append(i) if len(a)==0:     print('-1') else:     print(*a,sep='')

---
+++ Common_characters_-1.py
@@ -0,0 +1,16 @@
+s=input()
+s=str.lower(s)
+s=s.split()
+a=[]
+s1=s[0]
+for i in s1:
+    c=0
+    for j in s:
+        if i in j:
+            c+=1
+    if c==len(s):
+        a.append(i)
+if len(a)==0:
+    print('-1')
+else:
+    print(*a,sep='')
