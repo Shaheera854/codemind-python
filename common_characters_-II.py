38a9d171424aa887f59b146d7cf8503a97e72d60
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-07-19 21:24:11 +0530

    s1=input() s2=input() s1=s1.replace(' ','') s2=s2.replace(' ','') s1=str.lower(s1) s2=str.lower(s2) s=[] for i in s1:     if i in s2 and i not in s:         s.append(i) for i in s2:     if i in s1 and i not in s:         s.append(i) print(len(s))

---
+++ common_characters_-II.py
@@ -0,0 +1,14 @@
+s1=input()
+s2=input()
+s1=s1.replace(' ','')
+s2=s2.replace(' ','')
+s1=str.lower(s1)
+s2=str.lower(s2)
+s=[]
+for i in s1:
+    if i in s2 and i not in s:
+        s.append(i)
+for i in s2:
+    if i in s1 and i not in s:
+        s.append(i)
+print(len(s))
