ff700026d346be944d09746fee6c90ab4652c63e
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-07-19 21:00:07 +0530

    a=input() a=a.replace(' ','') a=str.lower(a) a=set(a) print(*sorted(list(a)),sep='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,11 +1,5 @@
-s=input()
-s1=''
-s=s.replace(' ','')
-for i in s:
-    c=0
-    for j in s:
-        if i==j or ord(i)+32==ord(j) or ord(i)-32==ord(j) and i!=' ' and j!=' ':
-            c+=1
-    if c==1:
-        s1+=i
-print(*sorted(str.lower(s1)),sep='')
+a=input()
+a=a.replace(' ','')
+a=str.lower(a)
+a=set(a)
+print(*sorted(list(a)),sep='')
