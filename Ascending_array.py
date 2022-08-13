af9c224652a2eaae2507ff526d7612cb1f8cd5e6
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-13 19:32:23 +0530

    n=int(input()) a=list(map(int,input().split())) b=set(a) if a==sorted(list(b)):     print("yes") else:     print("no")

--- Ascending_array.py
+++ Ascending_array.py
@@ -1,8 +1,7 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range(n-1):
-    if a[i]>=a[i+1]:
-        print("no")
-        break
-else:
+b=set(a)
+if a==sorted(list(b)):
     print("yes")
+else:
+    print("no")
