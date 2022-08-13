2fd3aee4b19c4c2e293ba1111ae0af6986e4a1c8
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-13 17:39:29 +0530

    m,n=map(int,input().split()) l=[] for i in range(m):     a=list(map(int,input().split()))     l.append(a) se=0 so=0 for i in range(m):     for j in range(n):         if l[i][j]%2==0:             se+=l[i][j]         else:             so+=l[i][j] print(se,so)

--- Sum_of_even_and__odd_elements_in_a_matrix.py
+++ Sum_of_even_and__odd_elements_in_a_matrix.py
@@ -1,14 +1,14 @@
 m,n=map(int,input().split())
-a=[]
+l=[]
 for i in range(m):
-    x=list(map(int,input().split()))
-    a.append(x)
+    a=list(map(int,input().split()))
+    l.append(a)
 se=0
 so=0
 for i in range(m):
     for j in range(n):
-        if a[i][j]%2==0:
-            se+=a[i][j]
+        if l[i][j]%2==0:
+            se+=l[i][j]
         else:
-            so+=a[i][j]
+            so+=l[i][j]
 print(se,so)
