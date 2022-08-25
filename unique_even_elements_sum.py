cf3a01e44228132ef2857cee17f68d09affb6670
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-25 23:32:05 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 a=set(a) a=list(a) for i in range(len(a)):     c=0     for j in range(len(a)):         if a[i]==a[j]:             c+=1     if c<=1 and a[i]%2==0:         s+=a[i] print(s)

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+a=set(a)
+a=list(a)
+for i in range(len(a)):
+    c=0
+    for j in range(len(a)):
+        if a[i]==a[j]:
+            c+=1
+    if c<=1 and a[i]%2==0:
+        s+=a[i]
+print(s)
