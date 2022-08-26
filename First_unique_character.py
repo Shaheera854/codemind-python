9ad6b6cfa2bd5b464b46b3095b37e1f5bfb8e5e5
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 22:34:08 +0530

    s=input() s=s.lower() l=0 for i in range(len(s)):     c=0     for j in range(len(s)):         if s[i]==s[j] and s[i]!=' ':             c+=1     if c==1:         print(s[i])         break else:     print("-1")

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,13 +1,13 @@
 s=input()
-s1=0
-s=s.replace(' ','')
-for i in s:
+s=s.lower()
+l=0
+for i in range(len(s)):
     c=0
-    for j in s:
-        if i==j or ord(i)+32==ord(j) or ord(i)-32==ord(j) and i!=' ' and j!=' ':
+    for j in range(len(s)):
+        if s[i]==s[j] and s[i]!=' ':
             c+=1
     if c==1:
-        print(i)
-        exit()
+        print(s[i])
+        break
 else:
-    print('-1')
+    print("-1")
