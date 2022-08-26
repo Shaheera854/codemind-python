346200629bce7fc3a9e4de4b7fcd522af49a9dfb
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 00:32:24 +0530

    n=input().split() s_min=0 s_max=0 for i in n:     mini=ord(i[0])     maxi=ord(i[0])     for j in range(1,len(i)):         if mini>ord(i[j]) and i[j].isalnum():             mini=ord(i[j])         if maxi<ord(i[j]) and i[j].isalnum():             maxi=ord(i[j])     print(chr(mini),chr(maxi),end=' ')

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,3 +1,12 @@
-s=input().split()
-for i in s:
-    print(min(i),max(i),end=' ')
+n=input().split()
+s_min=0
+s_max=0
+for i in n:
+    mini=ord(i[0])
+    maxi=ord(i[0])
+    for j in range(1,len(i)):
+        if mini>ord(i[j]) and i[j].isalnum():
+            mini=ord(i[j])
+        if maxi<ord(i[j]) and i[j].isalnum():
+            maxi=ord(i[j])
+    print(chr(mini),chr(maxi),end=' ')
