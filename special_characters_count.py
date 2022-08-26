88963f105ebf8e160b35ab7da07babcc14a43239
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 00:59:14 +0530

    s=input() c=0 for i in s:    if i.isalnum() or i==' ':        continue    else:        c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,7 +1,8 @@
 s=input()
 c=0
 for i in s:
-    j=ord(i)
-    if (j>32 and j<=47) or (j>=58 and j<=64) or (j>=91 and j<=96) or (j>=123 and j<=126):
-        c+=1
+   if i.isalnum() or i==' ':
+       continue
+   else:
+       c+=1
 print(c)
