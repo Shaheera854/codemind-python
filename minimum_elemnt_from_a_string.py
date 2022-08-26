19bcd43fa5445e452e03f6ddd65cb0e709b90a78
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 00:42:37 +0530

    s=list(map(str,input().split())) s=s[len(s)-1] n=min(s) if s.count(n.lower())!=0:     print(n.lower()) else:     print(n)

--- minimum_elemnt_from_a_string.py
+++ minimum_elemnt_from_a_string.py
@@ -1,11 +1,7 @@
-s=input().split()
+s=list(map(str,input().split()))
 s=s[len(s)-1]
-s1=''
-for i in s:
-    if str.isalpha(i):
-        s1+=i
-m=min(s1)
-if str.isupper(m) and str.lower(m) in s1:
-    print(str.lower(m))
+n=min(s)
+if s.count(n.lower())!=0:
+    print(n.lower())
 else:
-    print(m)
+    print(n)
