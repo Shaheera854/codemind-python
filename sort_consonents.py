9611190280dd2a1c9b89395f680d206b6610e0dd
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 23:11:58 +0530

    n=list(map(str,input().split())) s='' vowels=['a','e','i','o','u','A','E','I','O','U'] for i in n:     l=list(i)     for x in range(len(l)):         for y in range(len(l)):             if x!=y and l[x] not in vowels and l[y] not in vowels and l[x]<l[y]:                 temp=l[x]                 l[x]=l[y]                 l[y]=temp     print(*l,sep='',end=' ')

--- sort_consonents.py
+++ sort_consonents.py
@@ -1,8 +1,13 @@
-s=input().split()
-for k in s:
-    a=list(k)
-    for i in range(len(a)):
-        for j in range(i+1,len(a)):
-            if a[i]>a[j] and a[i] not in 'aeiou' and a[j] not in 'aeiou':
-                a[i],a[j]=a[j],a[i]
-    print(*a,sep='',end=' ')
+n=list(map(str,input().split()))
+s=''
+vowels=['a','e','i','o','u','A','E','I','O','U']
+for i in n:
+    l=list(i)
+    for x in range(len(l)):
+        for y in range(len(l)):
+            if x!=y and l[x] not in vowels and l[y] not in vowels and l[x]<l[y]:
+                temp=l[x]
+                l[x]=l[y]
+                l[y]=temp
+    print(*l,sep='',end=' ')
+    
