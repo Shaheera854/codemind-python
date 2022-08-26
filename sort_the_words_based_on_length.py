695c86a11bef369e36e078a4f91cdb1f9bffa1f8
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 21:55:52 +0530

    s=list(map(str,input().split())) for i in range(len(s)):     for j in range(len(s)):         if i!=j and len(s[i])<len(s[j]):             temp=s[i]             s[i]=s[j]             s[j]=temp print(*s)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,22 +1,8 @@
-s=input().split()
-i=0
-j=1
-while i<len(s) and j<len(s):
-    if len(s[i])==len(s[j]):
-        a1=0
-        a2=0
-        for k in s[i]:
-            a1+=ord(k)
-        for k in s[j]:
-            a2+=ord(k)
-        if a1>a2:
+s=list(map(str,input().split()))
+for i in range(len(s)):
+    for j in range(len(s)):
+        if i!=j and len(s[i])<len(s[j]):
             temp=s[i]
             s[i]=s[j]
             s[j]=temp
-    elif len(s[i])>len(s[j]):
-        temp=s[i]
-        s[i]=s[j]
-        s[j]=temp
-    j+=1
-    i+=1
 print(*s)
