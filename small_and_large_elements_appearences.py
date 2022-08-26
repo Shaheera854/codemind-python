028ddd607a3adf6304ac1797726a105888114e68
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 00:26:45 +0530

    n=input() s_min=0 s_max=0 mini=ord(n[0]) maxi=ord(n[0]) for j in range(1,len(n)):     if mini>ord(n[j]) and n[j].isalnum():         mini=ord(n[j])     if maxi<ord(n[j]) and n[j].isalnum():         maxi=ord(n[j]) print(chr(mini),n.count(chr(mini)),chr(maxi),n.count(chr(maxi)))

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,15 +1,11 @@
-s=input()
-s1=''
-c1=0
-c2=0
-for i in s:
-    if str.isalpha(i):
-        s1+=i
-maxi=max(s1)
-mini=min(s1)
-for i in s1:
-    if maxi==i:
-        c1+=1
-    if mini==i:
-        c2+=1
-print(mini,c2,maxi,c1)
+n=input()
+s_min=0
+s_max=0
+mini=ord(n[0])
+maxi=ord(n[0])
+for j in range(1,len(n)):
+    if mini>ord(n[j]) and n[j].isalnum():
+        mini=ord(n[j])
+    if maxi<ord(n[j]) and n[j].isalnum():
+        maxi=ord(n[j])
+print(chr(mini),n.count(chr(mini)),chr(maxi),n.count(chr(maxi)))
