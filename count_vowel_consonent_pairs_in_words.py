54e601f6c6efeaa6797ae61b7a6a5eaaa53475cb
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 00:46:35 +0530

    s=input().split() v='aeiouAEIOU' c=0 for i in s:     x=0     y=len(i)-1     while x<y:         if ((i[x] in v and i[y] not in v) or (i[x] not in v and i[y] in v)):             c+=1         x+=1         y-=1 print(c)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -1,15 +1,12 @@
 s=input().split()
-vowels=['a','e','i','o','u','A','E','I','O','U']
-consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+v='aeiouAEIOU'
 c=0
-for k in s:
-    i=0
-    j=len(k)-1
-    while i<(len(k)//2) and j>=(len(k)//2):#shaheera
-        if k[i] in vowels and k[j] in consonants:
+for i in s:
+    x=0
+    y=len(i)-1
+    while x<y:
+        if ((i[x] in v and i[y] not in v) or (i[x] not in v and i[y] in v)):
             c+=1
-        elif k[i] in consonants and k[j] in vowels:
-            c+=1
-        i+=1
-        j-=1
+        x+=1
+        y-=1
 print(c)
