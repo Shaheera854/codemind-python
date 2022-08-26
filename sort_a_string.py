18b23483a0eea30ec93ff185ce3ac7786ada921c
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 23:56:31 +0530

    n=input() n=list(n) for i in range(len(n)):     for j in range(len(n)):         if i!=j and n[i].isalnum() and n[j].isalnum() and n[i]<n[j]:             temp=n[i]             n[i]=n[j]             n[j]=temp print(*n,sep='')

--- sort_a_string.py
+++ sort_a_string.py
@@ -1,7 +1,9 @@
-s=input()
-s=list(s)
-for i in range(len(s)):
-    for j in range(i+1,len(s)):
-        if s[i]>s[j] and s[i] not in '!@#$%^&*(){}[]''"":;/?.>,<\|+=-_ ' and s[j] not in '!@#$%^&*(){}[]''"":;/?.>,<\|+=-_ ':
-            s[i],s[j]=s[j],s[i]
-print(*s,sep='',end=' ')
+n=input()
+n=list(n)
+for i in range(len(n)):
+    for j in range(len(n)):
+        if i!=j and n[i].isalnum() and n[j].isalnum() and n[i]<n[j]:
+            temp=n[i]
+            n[i]=n[j]
+            n[j]=temp
+print(*n,sep='')
