e65f095d0741172dd57897e996ad57396e7f3f75
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 22:03:26 +0530

    s=input() s=s.lower() l=[] for i in range(len(s)):     c=0     for j in range(len(s)):         if s[i]==s[j] and s[i]!=' ':             c+=1     if c==1:         l.append(s[i]) l=sorted(l) for i in l:     print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,5 +1,13 @@
-a=input()
-a=a.replace(' ','')
-a=str.lower(a)
-a=set(a)
-print(*sorted(list(a)),sep='')
+s=input()
+s=s.lower()
+l=[]
+for i in range(len(s)):
+    c=0
+    for j in range(len(s)):
+        if s[i]==s[j] and s[i]!=' ':
+            c+=1
+    if c==1:
+        l.append(s[i])
+l=sorted(l)
+for i in l:
+    print(i,end='')
