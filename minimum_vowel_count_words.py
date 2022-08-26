daf14224a9ba71993ac2f426491ed06d29c997ff
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 01:04:16 +0530

    s=input().lower().split() m=99999 c=0 for i in range(len(s)):     d=0     for j in s[i]:         if j in 'aeiou':             d+=1     if d<m:         m=d for i in range(len(s)):     d=0     for j in s[i]:         if j in 'aeiou':             d+=1     if d==m:         c+=1 print(c)

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,26 +1,18 @@
-def min_count(s):
-    s=s.split()
-    v=['a','e','i','o','u','A','E','I','O','U']
-    c=0
-    for i in range(len(s)):
-        m=c
-        c=0
-        for j in range(len(s[i])):
-            if s[i][j] in v:
-                c+=1
-        if c>m and m!=0:
-            c=m
-    return c
-s=input()
-m=min_count(s)
-s=s.split()
-co=0
-v=['a','e','i','o','u','A','E','I','O','U']
+s=input().lower().split()
+m=99999
+c=0
 for i in range(len(s)):
-    c=0
-    for j in range(len(s[i])):
-        if s[i][j] in v:
-            c+=1
-    if c==m:
-        co+=1
-print(co)
+    d=0
+    for j in s[i]:
+        if j in 'aeiou':
+            d+=1
+    if d<m:
+        m=d
+for i in range(len(s)):
+    d=0
+    for j in s[i]:
+        if j in 'aeiou':
+            d+=1
+    if d==m:
+        c+=1
+print(c)
