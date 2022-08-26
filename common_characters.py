018a26cfffeaa834edabd69e81d9cb61935b1132
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 23:51:04 +0530

    s1=input().lower() s2=input().lower() s='' for i in s1:     if i in s2 and i!=' ' and i not in s:         s+=i if len(s)==0:     print("-1") else:     print(*sorted(list(s)),sep='')

--- common_characters.py
+++ common_characters.py
@@ -1,14 +1,10 @@
-s1=input()
-s2=input()
-s1=s1.replace(' ','')
-s2=s2.replace(' ','')
-s1=str.lower(s1)
-s2=str.lower(s2)
-s=[]
+s1=input().lower()
+s2=input().lower()
+s=''
 for i in s1:
-    if i in s2 and i not in s:
-        s.append(i)
+    if i in s2 and i!=' ' and i not in s:
+        s+=i
 if len(s)==0:
-    print('-1')
+    print("-1")
 else:
-    print(*sorted(s),sep='')
+    print(*sorted(list(s)),sep='')
