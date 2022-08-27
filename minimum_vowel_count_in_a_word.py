9987bae63a1437f0357959e9b25efa522358fb4e
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 11:56:02 +0530

    s=input().lower().split() vowels='aeiouAEIOU' mini=0 for i in s:     c=0     for j in range(len(i)):         if i[j] in vowels:             c+=1     if mini==0 or mini>c:         mini=c print(mini)

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,15 +1,11 @@
-def min_count(s):
-    s=s.split()
-    v=['a','e','i','o','u','A','E','I','O','U']
+s=input().lower().split()
+vowels='aeiouAEIOU'
+mini=0
+for i in s:
     c=0
-    for i in range(len(s)):
-        m=c
-        c=0
-        for j in range(len(s[i])):
-            if s[i][j] in v:
-                c+=1
-        if c>m and m!=0:
-            c=m
-    return c
-s=input()
-print(min_count(s))
+    for j in range(len(i)):
+        if i[j] in vowels:
+            c+=1
+    if mini==0 or mini>c:
+        mini=c
+print(mini)
