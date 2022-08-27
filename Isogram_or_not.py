91dc68479e9b2667defa243fce6d7fea9bd3a42b
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 12:12:50 +0530

    s=input() if len(list(s))==len(set(s)):     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,11 +1,5 @@
 s=input()
-for i in range(len(s)):
-    c=0
-    for j in range(len(s)):
-        if s[i]==s[j]:
-            c+=1
-    if c>1:
-        print(False)
-        break
-else:
+if len(list(s))==len(set(s)):
     print(True)
+else:
+    print(False)
