cc61d0c3da789ecbd0ec67b6227d001611402e68
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-27 14:02:48 +0530

    n=input().split() l1=[] for i in range(len(n)):     if i%2==0:         l1.append(n[i][-1::-1])     else:         l1.append(n[i]) print(*l1)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,5 +1,8 @@
-s=input()
-s=s.split()
-for i in range(0,len(s),2):
-    s[i]=s[i][::-1]
-print(*s)
+n=input().split()
+l1=[]
+for i in range(len(n)):
+    if i%2==0:
+        l1.append(n[i][-1::-1])
+    else:
+        l1.append(n[i])
+print(*l1)
